50f1a79d9187b5477ca9089226ceeaacdbf1d315
David Hempy <dhempy@appriss.com>
2021-12-03 09:08:02 -0500

    Day 03a

---
+++ 03a/03a.py
@@ -0,0 +1,45 @@
+#! /usr/local/bin/python3
+filename = "power.txt"
+# filename = "tiny_power.txt"
+file = open(filename, "r")
+
+ones = []
+lines = 0
+line_num = 0
+bit_count = -1
+
+for power in file:
+  # print(power.rstrip())
+  lines += 1
+
+  if bit_count < 0:
+    bit_count = len(power)-1
+    # print(f"Initialize array to {bit_count} items.")
+    ones = [0] * bit_count
+    # print(ones)
+
+  for index, bit in enumerate(power):
+    # print(f"{index}: {bit}")
+    if bit == '1':
+      # print("increment")
+      ones[index] += 1
+
+    # print(ones)
+
+gamma_rate = 0
+epsilon_rate = 0
+threshold = int(lines / 2)
+
+for one_count in ones:
+  gamma_rate *= 2
+  epsilon_rate *= 2
+  if one_count >= threshold:
+    gamma_rate += 1
+  else:
+    epsilon_rate += 1
+
+print(f"gamma_rate: {gamma_rate}")
+print(f"epsilon_rate: {epsilon_rate}")
+
+total_power = gamma_rate * epsilon_rate
+print(f"total_power: {total_power}")
