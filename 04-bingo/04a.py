225f0f1d25a475a7a8e002f8b1bc81d5f8eacb98
David Hempy <dhempy@appriss.com>
2021-12-07 22:22:28 -0500

    Day 4a: Winning board selected.

--- 04-bingo/04a.py
+++ 04-bingo/04a.py
@@ -1,28 +1,48 @@
 #! /usr/local/bin/python3
-filename = "input-test.txt"
+# filename = "input-test.txt"
+filename = "input-solo.txt"
 # filename = "input.txt"
 
-def cheat(picks, boards):
-  return 13, 12345678
+def find_winner(draws, boards):
+  for draw in draws:
+    for board in boards:
+      if board.play_wins(draw): return board
+  return None
 
 class Board:
   def __init__(self, name, lines):
+    print(f"Board.init({name}, {lines})...")
     self.name = name
     self.lines = lines
 
   def dump(self):
-    print(f"Board {self.name}: {len(self.lines)} lines")
+    print(f"Board {self.name}: score={self.score()}")
     for line in self.lines:
       print(f"  {line}")
 
+  def score(self):
+    return 987654321
+
+  # plays a number (draw) on the board.
+  # returns true if that draw was a winner.
+  def play_wins(self, draw):
+    print(f"Play {draw} on board {self.name}: ")
+    self.dump()
+    winner = False
+    for line in self.lines:
+      if draw in line:
+        line.remove(draw)
+        if not line: winner = True # but still need to remove draw from the other line.
+    return winner
 
 def slurp(filename):
   file = open(filename)
-  picks = file.readline().split(',')
+  draws = [int(i) for i in file.readline().split(',')]
   file.readline()
-  print(f"picks: {picks}")
+  # print(f"draws: {draws}")
 
   boards = []
+  index = 0
   for grid in file.read().split('\n\n'):
     # print(f"grid: \n{grid}\n")
     string = ' '.join(grid.split('\n'))
@@ -34,24 +54,22 @@ def slurp(filename):
     cols = [flat[start::5] for start in range(0, 5)]
     # print(f"cols: {cols}\n")
     lines = rows + cols
-    print(f"lines: {lines}\n")
-    boards.append(Board("John", lines))
+    # print(f"lines: {lines}\n")
+    boards.append(Board(index, lines))
+    index += 1
 
-  print(f"boards: {boards}")
-
-  return picks, boards
+  return draws, boards
 
 def main():
-  picks, boards = slurp(filename)
-  for board in boards:
-    board.dump()
-    # print(f"Board {board.name}: {len(board.lines)} lines: {board.lines} ")
-  # print(boards)
+  draws, boards = slurp(filename)
+  [board.dump() for board in boards]
 
-  winner, score = cheat(picks, boards)
+  winner = find_winner(draws, boards)
 
+  print(f"The winning board:")
+  winner.dump()
   print(f"The winning board is {winner}, with a score of {score}")
-  assert winning_score == 4512, "NOPE! Winning score should be 4512"
+  assert winner.score == 4512, "NOPE! Winning score should be 4512"
   # life_support_report(boards)
 
 main()
