778686e8fc401449230f8f8634a1395ac790a1b7
David Hempy <dhempy@jellyvision.com>
2021-12-15 02:25:04 -0500

    Day 14: Complete. Switched to pair counting.

--- 14-polymers/14b.py
+++ 14-polymers/14b.py
@@ -1,15 +1,17 @@
 #! /usr/local/bin/python3
-filename = "input-1588"
+# filename = "input-1588"
+# filename = "input-2188189693529"
 # filename = "input-2x40"
-# filename = "input"
+# filename = "input-8x5"
+filename = "input"
 
 from collections import Counter
 import re
 
-# debug = False
-debug = True
+debug = False
+# debug = True
 
-max_days = 10
+max_days = 40
 
 import math
 def log(m):
@@ -22,14 +24,17 @@ class Rule:
     self.insert = insert
     new_chars = [target[0], insert, target[1]]
     log(f"rule.new_chars: {new_chars}")
-    self.new_pairs = Counter([new_chars[0,1], new_chars[1,2])])
-    log(f"rule.new_pairs: {new_pairs}")
+    self.new_pairs = [''.join(new_chars[0:2]), ''.join(new_chars[1:3])]
+    log(f"rule.pairs: {self.new_pairs}")
+
+  # def new_pairs(self, qty):
+  #   return Counter({self.pairs[0]: qty, self.pairs[1]: qty})
 
   def dump(self):
     log(self.summary())
 
   def summary(self):
-    f" {self.target} gets {self.insert}"
+    return f" {self.target} spawns {self.new_pairs}"
 
 
 
@@ -40,9 +45,7 @@ class Board:
     self.today = 0
     self.slurp()
     self.pairs = Counter()
-    self.init_pairs
-    self.foresites = {}
-    self.foresites_init()
+    self.init_pairs()
     self.dump()
 
   def slurp(self):
@@ -54,52 +57,79 @@ class Board:
 
     self.rules = []
     for line in file:
+      log(line)
       target, insert = line.rstrip().split(' -> ')
       self.rules.append(Rule(target, insert))
 
   def dump(self):
+    log('')
     log(self.summary())
     for rule in self.rules:
       rule.dump()
     log(f'counts: {self.pairs} ')
+    log('')
 
   def summary(self):
     return f"Board {self.filename}:"
 
   def init_pairs(self):
+    log('\ninit_pairs()...')
     word = f"{self.word}$" # '$' to protect last character
+    log(f'word:{word}')
     for i in range(len(word)-1):
-      self.pairs += self.word[i:i+2]
+      pair = word[i:i+2]
+      self.pairs[pair] += 1
+
+    log(f'self.pairs:{self.pairs}')
+
+  def find_rule(self, pair):
+    matches = [rule for rule in self.rules if rule.target == pair]
+    if matches:
+      return matches[0]
+    return False
+
+  def advance_pair(self, old_pair, qty):
+    log(f' advance_pair({old_pair}, {qty})... old_pair is {type(old_pair)}')
+    rule = self.find_rule(old_pair)
+    if not rule:
+      log('  preserve')
+      return
+    rule.dump()
+
+    log(f'  self.pairs: {self.pairs}')
+    for new_pair in rule.new_pairs:
+      log(f'   increment {new_pair} by {qty}')
+      self.pairs[new_pair] += qty
+
+    log(f'  self.pairs: {self.pairs}')
+    log(f'   decrement {old_pair} by {qty}')
+    self.pairs[old_pair] -= qty
+    log(f'  self.pairs: {self.pairs}')
 
   def advance(self):
     log(f"================================== advance from day {self.today}:")
     self.today += 1
     # print(f"{self.today}:  PRE: {self.word}")
 
-    for i in range(len(self.word)-1):
-      pair = self.word[i:i+2]
-      # log(f' pair: {pair}')
-      # log(f' append frst {pair[0]} ')
-      next_word.append(pair[0])
-      for rule in self.rules:
-        if pair == rule.target:
-          log(f'    {rule.target} -> {rule.insert}')
-          next_word.append(rule.insert)
-          break
-
-    # next_word.append(self.word[-1])
-    # self.word = ''.join(next_word)
-    # print(f"{self.today}: POST: {len(self.word)}  {self.word}")
+    new_pairs = Counter()
+    for pair, count in self.pairs.most_common():
+      self.advance_pair(pair, count)
 
   def score(self):
-    log(self.pairs)
-    max = self.pairs.most_common()[0][1]
-    min = self.pairs.most_common()[-1][1]
-    return max - min
+    print('\nscore():')
+    print(self.pairs)
+    letters = Counter()
+    for pair, qty in self.pairs.most_common():
+      letters[pair[0]] += qty
+    print(letters)
+    max = letters.most_common()[0]
+    min = letters.most_common()[-1]
+    score = max[1] - min[1]
+    print(f' {max} - {min} => {score}')
+    return score
 
 def main():
   board = Board(filename)
-  board.dump()
 
   print(f"0: POST: {len(board.word)}  {board.word}")
 
