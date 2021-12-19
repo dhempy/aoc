a131165050efb9de377d24e18dd39eeaa2f25d0d
David Hempy <dhempy@jellyvision.com>
2021-12-18 23:46:29 -0500

    Day 18a: Exploder working. Reduce calling all exploders before any splits.

--- 18-snailfish-math/18a.py
+++ 18-snailfish-math/18a.py
@@ -1,9 +1,7 @@
 #! /usr/local/bin/python3
 filename = "in-full"
-filename = "in-1"
-filename = "in-7"
-filename = "in-2"
-filename = "in-8"
+filename = "explode-2"
+# filename = "in-13"
 
 # debug = False
 debug = True
@@ -56,99 +54,26 @@ class Board:
     else:
       self.expected_magnitude = False
 
-  def split(self, item):
-    log(f'   TOO HIGH! {item}...SPLIT!')
-    return f'split-{item}'
 
-  def explode(self, item, depth, src):
-    # To explode a pair, the pair's left value is added to the first regular
-    # number to the left of the exploding pair (if any), and the pair's
-    # right value is added to the first regular number to the right of the
-    # exploding pair (if any). Exploding pairs will always consist of two
-    # regular numbers. Then, the entire exploding pair is replaced with the
-    # regular number 0.
+  def magnitude(self, item):
+    # The magnitude of a pair is
+    #   3 times the magnitude of its left element
+    #   plus
+    #   2 times the magnitude of its right element.
+    # The magnitude of a regular number is just that number.
 
-    log(f'   TOO DEEP...EXPLODE! {item}, depth=>{depth}, src=>{src}')
-    item[0] = -item[0]
-    item[1] = -item[1]
-    # log(f'    item[0] => {item[0]} ')
-    # log(f'    item[1] => {item[1]} ')
-    # log(f"     {self.to_s(src)}")
-    # log(f"     split({f'[{item[0]},{item[1]}]'}):")
-    # log(self.to_s(src).split() )
-    # log( self.to_s(src).split(f'[{item[0]},{item[1]}]'))
-    # log('split on comma:')
-    # log( self.to_s(src).split(','))
-    pre, post = self.to_s(src).split(f'[{item[0]},{item[1]}]')
-    item[0] = -item[0]
-    item[1] = -item[1]
-
-    log(f'{pre} -- (bomb) -- {post} ')
-
-    matches = re.match(r'(\D*)(\d)(.*)', post)
-    if matches:
-      log(f' post matches: {matches}')
-      log(f' post matches[1]: {matches[1]}')
-      log(f' post matches[2]: {matches[2]}')
-      log(f' post matches[3]: {matches[3]}')
-      new_val = int(matches[2]) + int(item[1])
-      post = f'{matches[1]}{new_val}{matches[3]}'
-    else:
-      log(f' post no matches')
-
-    matches = re.match(r'(.*)(\d)(\D*)', pre)
-    if matches:
-      log(f' PRE matches: {matches}')
-      log(f' PRE matches[1]: {matches[1]}')
-      log(f' PRE matches[2]: {matches[2]}')
-      log(f' PRE matches[3]: {matches[3]}')
-      new_val = int(matches[2]) + int(item[1])
-      pre = f'{matches[1]}{new_val}{matches[3]}'
-    else:
-      log(f' PRE no matches')
-
-    log(f'{pre} -- (bomb) -- {post} ')
-
-    final = f'{pre}0{post}'
-    # pre = re.sub(r'(.*)(\d)', lambda m: sum(m.group()), string)
-    # lambda m: myfunction(m.group())
-    # pre = re.sub(patterg, newtext, string)
-
-    return final
-
-  def reduce(self, item, depth=0, src=False):
-    # To reduce a snailfish number, you must repeatedly do the first action
-    # in this list that applies to the snailfish number:
-    #  - If any pair is nested inside four pairs, the leftmost such pair explodes.
-    #  - If any regular number is 10 or greater, the leftmost such regular number splits.
-    #
-    # If nothing reduced, it returns false.
-    # Else it returns a replacement for the *entire* number (not just this item)
-
-    src = src or item
-
+    # print(f'type of item is {type(item)} ')
     if type(item) == int:
-      if item >= 10:
-        return self.split(item, depth)
-      else:
-        return False
+      return item
+    log(f'magnitude({item}) ')
+    left, right = item[0:2]
+    # log(f'adding 3*{left} and 2*{right}...')
+    mag = 3*self.magnitude(left) + 2*self.magnitude(right)
+    # log(f' ... to get: {mag}')
+    return mag
 
-    log(f'\n  reduce({item}, depth=>{depth}, src=>{src})')
 
-    if depth >= 4:
-      return self.explode(item, depth, src)
 
-    log(f'             item: "{item}" ')
-    return self.reduce(item[0], depth+1, src) or
-           self.reduce(item[1], depth+1, src)
-
-  def add(self, a, b):
-    log(f'\n add({a}, {b}) ')
-    print('TODO: reduce() should scan the whole number for explosions before looking for any splits.')
-
-    a = self.reduce(a)
-    b = self.reduce(b)
-    return [a,b]
 
   def sum(self):
     log('sum()...')
@@ -160,24 +85,111 @@ class Board:
         sum = self.add(sum, a)
       else:
         log(' sum().intializing...')
-        sum = self.reduce(a)
+        sum = self.reduce_number(a)
     self.final_sum = sum
     return self.to_s(sum)
 
-  def magnitude(self, item = False):
-    # The magnitude of a pair is 3 times the magnitude of its left element
-    # plus 2 times the magnitude of its right element. The magnitude of a
-    # regular number is just that number.
-    item = item or self.final_sum
-    # print(f'type of item is {type(item)} ')
+
+  def add(self, a, b):
+    log(f'\n add({a}, {b}) ')
+
+    a = self.reduce_number(a)
+    b = self.reduce_number(b)
+    return [a,b]
+
+  def reduce_number(self, number):
+    current_number = number
+    while True:
+      log(f' current: {current_number}')
+      reduced = self.reduce_item_exploder(current_number) or \
+                self.reduce_item_splitter(current_number)
+      if not reduced:
+        break
+      current_number = reduced
+    return current_number
+
+
+  def reduce_item_exploder(self, item, depth=0, src=False):
+    # To reduce a snailfish number, you must repeatedly do the first action
+    # in this list that applies to the snailfish number:
+    #  - If any pair is nested inside four pairs, the leftmost such pair explodes.
+    #  - If any regular number is 10 or greater, the leftmost such regular number splits.
+    #
+    # If nothing reduced, it returns false.
+    # Else it returns a replacement for the *entire* number (not just this item) (as a deep array of items)
+
+    src = src or item
+
     if type(item) == int:
-      return item
-    # log(f'magnitude({item}) ')
-    left, right = item[0:2]
-    # log(f'adding 3*{left} and 2*{right}...')
-    mag = 3*self.magnitude(left) + 2*self.magnitude(right)
-    # log(f' ... to get: {mag}')
-    return mag
+      return False
+
+    # log(f'\n  reduce_item_exploder({item}, depth=>{depth}, src=>{src})')
+
+    if depth >= 4:
+      return self.explode(item, depth, src)
+
+    # log(f'             item: "{item}" ')
+    return self.reduce_item_exploder(item[0], depth+1, src) or \
+           self.reduce_item_exploder(item[1], depth+1, src)
+
+  def reduce_item_splitter(self, item, src=False):
+    #  - If any regular number is 10 or greater, the leftmost such regular number splits.
+    #
+    # If nothing reduced, it returns false.
+    # Else it returns a replacement for the *entire* number (not just this item) (as a deep array of items)
+
+    log(f'  reduce_item_splitter({item})...')
+
+    src = src or item
+
+    if type(item) == int:
+      if item >= 10:
+        return self.split(item, depth)
+      else:
+        return False
+
+  def split(self, item):
+    # To split a regular number, replace it with a pair;
+    # the left element of the pair should be the regular number divided by two and rounded down,
+    # while the right element of the pair should be the regular number divided by two and rounded up.
+    # For example, 10 becomes [5,5], 11 becomes [5,6], 12 becomes [6,6], and so on.
+    log(f'   TOO HIGH! {item}...SPLIT!')
+    return f'split-{item}'
+
+  def explode(self, item, depth, src):
+    # To explode a pair, the pair's left value is added to the first regular
+    # number to the left of the exploding pair (if any), and the pair's
+    # right value is added to the first regular number to the right of the
+    # exploding pair (if any). Exploding pairs will always consist of two
+    # regular numbers. Then, the entire exploding pair is replaced with the
+    # regular number 0.
+    # item is an array (typ. exactly one pair of ints.)
+    # returns an array, which is the new entire number from src, after item is exploded.
+
+    # log(f'   TOO DEEP...EXPLODE! {item}, depth=>{depth}, src=>{src}')
+    item[0] = -item[0]  # negative to ensure a unique match. All legit input is positive.
+    item[1] = -item[1]
+    pre, post = self.to_s(src).split(f'[{item[0]},{item[1]}]')
+    item[0] = -item[0]
+    item[1] = -item[1]
+
+    log(f' EXPLODE: {pre} <-- [{item[0]},{item[1]}] --> {post} ')
+
+
+    matches = re.match(r'(.*)(\d+)(\D*)', pre)
+    if matches:
+      new_val = int(matches[2]) + int(item[0])
+      pre = f'{matches[1]}{new_val}{matches[3]}'
+
+    matches = re.match(r'(\D*)(\d+)(.*)', post)
+    if matches:
+      new_val = int(matches[2]) + int(item[1])
+      post = f'{matches[1]}{new_val}{matches[3]}'
+
+    log(f' EXPLODE: {pre} <--   0   --> {post} ')
+    reduced_number = f'{pre}0{post}'
+    return eval(reduced_number)
+
 
   def to_s(self, s):
     return str(s).replace(' ','')
@@ -195,18 +207,19 @@ def main():
   board = Board(filename)
   board.dump()
   sum = board.sum()
-  magnitude = board.magnitude()
   print(f"\a")
   print(f"                     sum: {sum}")
   print(f"      board.expected_sum: {board.expected_sum}")
+  magnitude = board.magnitude(board.final_sum)
   print(f"               magnitude: {magnitude}")
   print(f"board.expected_magnitude: {board.expected_magnitude}\n")
-
 
   if board.expected_sum:
     assert sum == board.expected_sum, f"NOPE! the sum should be \n  {board.expected_sum}, not \n  {sum}"
 
   if board.expected_magnitude:
     assert magnitude == board.expected_magnitude, f"NOPE! the magnitude should be \n  {board.expected_magnitude}, not \n  {magnitude}"
+
+  print("NO ERRORS DETECTED!  :-D")
 main()
 
--- README.md
+++ README.md
@@ -11,5 +11,8 @@ PLEASE let me know any ways I can make cleaner programs. In particular, I feel l
 The Python Way, and wasting a lot of time building my own iterators and such, ignoring some 
 of the more graceful, powerful constructs of Python. Feedback is encouraged, and the more direct the better. 
 
+Keep in mind this is contest code, not production code. What you see here is not polished, COMPELTELY ignores edge cases, and frequently accepts abhorent abuses in the sake of fast completion of a single, specific input. 
+
 I intend to push my daily solutions only after the next day's problem is posted. 
 Feel free to use anything here for anything you want. 
+
